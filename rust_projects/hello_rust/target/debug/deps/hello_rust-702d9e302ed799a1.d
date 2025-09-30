02873c6fbee44c7c9b560be67bd1bd1f71f016a7
linusshih <123146236+linusshih@users.noreply.github.com>
2025-09-30 03:49:53 -0700

    Delete rust_projects/hello_rust directory

--- rust_projects/hello_rust/target/debug/deps/hello_rust-702d9e302ed799a1.d
+++
@@ -1,5 +0,0 @@
-/workspaces/ECE4301/hello_rust/target/debug/deps/hello_rust-702d9e302ed799a1.d: src/main.rs
-
-/workspaces/ECE4301/hello_rust/target/debug/deps/libhello_rust-702d9e302ed799a1.rmeta: src/main.rs
-
-src/main.rs:
